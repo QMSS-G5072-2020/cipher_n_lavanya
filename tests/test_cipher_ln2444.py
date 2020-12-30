ae5758b5d46081be76d72080b01c937cd654ab5f
Lavanya <ln2444@columbia.edu>
2020-12-29 22:49:56 -0500

    ran tests successfully

--- tests/test_cipher_ln2444.py
+++ tests/test_cipher_ln2444.py
@@ -3,3 +3,12 @@ from cipher_ln2444 import cipher_ln2444
 
 def test_version():
     assert __version__ == '0.1.0'
+    
+import pytest
+
+def test_cipher():
+    example = 'hello'
+    shift = 2
+    expected = 'jgnnq'
+    actual = cipher_ln2444.cipher(example, shift)
+    assert actual == expected
